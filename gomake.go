1dd1a7ba7ba60ccd06cfeaa7a3ebca10a10fcf3d
peter ducai <peter.ducai@telekom.com>
2017-10-07 22:43:33 +0200

    cleanup

--- gomake.go
+++ gomake.go
@@ -1,15 +1,80 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
 )
+
 
 func main() {
 	major := 0
-	minor := 0
-	build := 0
+	minor := 1
+	build := "07102017"
 
-	var version = fmt.Printf("%d.%d.%d", major, minor, build)
-
+	var version = fmt.Sprintf("%d.%d.%s", major, minor, build)
 	fmt.Printf("gomake version %s \n", version)
+
+	//check for existence of Gomakefile.yml
+	if _, err := os.Stat("Gomakefile.yml"); !os.IsNotExist(err) {
+		fmt.Println("found makefile...                   OK")
+	} else {
+		fmt.Println("makefile NOT FOUND...            ERROR")
+		return;
+	}
+
+	loadMakefile()
+
+	//if on windows, we want .exe extension
+	if runtime.GOOS == "windows" {
+		//BINOUTPUT = BINOUTPUT + ".exe"
+	}
+
+	//buildProgram()
+}
+
+func loadMakefile() {
+	fmt.Println("reading Makefile..")
+}
+
+func buildProgram() {
+	//go build [-o output] [-i] [build flags] [packages]
+	cmd := exec.Command("go", "build", "-o","gomake.bin","gomake.go")
+	log.Printf("Running command and waiting for it to finish...")
+	err := cmd.Run()
+	log.Printf("Command finished with error: %v", err)
+}
+
+func clean() {
+
+}
+
+func test() {
+
+}
+
+//checksumFile will create sha256 checksum of whole file
+func checksumFile() {
+	f, err := os.Open("file.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%x", h.Sum(nil))
+}
+
+//checksumString will create sha256 checksum of given string
+func checksumString(mystr string) {
+	sum := sha256.Sum256([]byte(mystr))
+	fmt.Printf("%x", sum)
 }
